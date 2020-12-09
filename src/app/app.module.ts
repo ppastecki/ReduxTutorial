64e0ff1e88daf9406bb37db0c72766f29082c1f6
Patryk <patryk@acme.com>
2020-12-09 19:34:34 +0100

    add ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { CounterModule } from './counter/counter.module';
 import { JediModule } from './jedi/jedi.module';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
     CounterModule,
     JediModule
   ],
